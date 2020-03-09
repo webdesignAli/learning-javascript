78138264cf7e8e854f57f345d9b838b97eb41074
unknown <alirazavi253@gmail.com>
2020-03-09 23:03:56 +0330

    typeof varibles

---
+++ inbox.js
@@ -0,0 +1,21 @@
+let number=100;
+console.log(number,typeof number);
+let points=18.50;
+console.log(points,typeof points);
+let username="Ali";
+console.log(username,typeof username);
+let is_marrid=false;
+console.log(is_marrid,typeof is_marrid);
+let test=null;
+console.log(test,typeof test);
+let undi;
+console.log(undi,typeof undi);
+let obj={
+    a:1,
+    b:2
+}
+console.log(obj,typeof obj);
+let func=function(){
+    return "Hello World !";
+}
+console.log(func,typeof func);
